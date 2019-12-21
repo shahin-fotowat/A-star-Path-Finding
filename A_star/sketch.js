81a2b345c57266cff25ad05296a0303bb109159f
Virtual6 <sfototwa@uci.edu>
2019-12-20 23:24:55 -0800

    A* now works and displays the shortest path

--- A_star/sketch.js
+++ A_star/sketch.js
@@ -1,22 +1,27 @@
 
-var rows = 5;  // Number of rows in the grid
-var cols = 5;  // Number of columns in the grid
-var grid = new Array(rows); // Create the grid
+var rows = 25;  // Number of rows in the grid
+var cols = 25;  // Number of columns in the grid
+var grid = new Array(cols); // Create the grid
 var openList   = []; //List of nodes that still need to be evaluated
 var closedList = []; //List of nodes that have finished being evaluated
 var canvas_width  = 400; //Width of the canvas
-var canvas_height = 400; //Height of the canvas
+var canvas_height = 400 ; //Height of the canvas
 var width_dist, height_dist; //vertical and horizontal distances between
                              //each node to be calculated
+var path = [];
 
 //-----------------------------------------------------------------------------
 // Remove the evaluated node from openList
 function remove_element(arr, element) {
     for(var i = arr.length - 1; i >= 0; i--) {
-        if(arr[i] === element) {
+        if(arr[i] == element) {
             arr.splice(i, 1)
         }
     }
+}
+//-----------------------------------------------------------------------------
+function heuristic(point_A, point_B) {
+    return dist(point_A.x, point_A.y, point_B.x, point_B.y);
 }
 //-----------------------------------------------------------------------------
 //function constructor
@@ -25,7 +30,8 @@ function Node(x_val, y_val) {
     this.y = y_val;  // Y-coordinate of the node
     this.f = 0;      // cost function
     this.g = 0;      // distance traveled between nodes
-    this.h = 0;      // huerisitc value of each node
+    this.h = 0;      // heuristic value of each node
+    this.p = undefined;   // parent node of an evaluated node
     this.neighbors = [];  // list of neighbors of each node
 
     //Sets the neighbors of each selected node
@@ -36,7 +42,7 @@ function Node(x_val, y_val) {
         if(this.x > 0) {
             this.neighbors.push(grid[this.x - 1][this.y]);
         }
-        if(this.y < rows - 1 ) {
+        if(this.y < rows - 1) {
             this.neighbors.push(grid[this.x][this.y + 1]);
         }
         if(this.y > 0) {
@@ -49,7 +55,7 @@ function Node(x_val, y_val) {
         fill(node_color);
         noStroke();
         rect(this.x * width_dist, this.y * height_dist,
-            width_dist - 1, height_dist - 1);
+             width_dist - 1, height_dist - 1);
     }
 }
 //-----------------------------------------------------------------------------
@@ -72,64 +78,81 @@ function setup() {
             grid[i][j] = new Node(i, j);
         }
     }
+
+    for(var i = 0; i < rows; i++) {
+        for(var j = 0; j < cols; j++) {
+            grid[i][j].setNeighbors(grid);
+        }
+    }
     starting_node = grid[0][0]; //Starting node top_left
     ending_node = grid[rows - 1][cols - 1]; // Ending node bottom right
 
     //start from the starting point added to the list
     openList.push(starting_node);
     console.log(grid);
-}
+
+} // end setup()
 //-----------------------------------------------------------------------------
 function draw() {
     background(0);
 
     if(openList.length > 0) {
-
-        lowest_cost = 0;  //index of the node with the loswest cost to the end
+        var lowest_cost = 0;  //index of the node with the loswest cost to the end
         //compares the temp lowest cost to the other node available in the
         //openList and finds the lowest of all
         for(var i = 0; i < openList.length; i++) {
-            if(openList[i] < openList[lowest_cost]) {
+            if(openList[i].f < openList[lowest_cost].f) {
                 lowest_cost = i;
             }
         }
+        var evaluated_node = openList[lowest_cost];
+
         //if the lowest cost found is the ending point, then we're finished
-        if(openList[lowest_cost] === ending_node) {
-            console.log("Searched is finished!");
-        } else {
+        if(evaluated_node === ending_node) {
+            noLoop();
+        } //else {
             //Add the neighbors of the evaluated node
-            openList[lowest_cost].setNeighbors(grid);
-        }
-        //Add the evaluated node to the closedList
-        closedList.push(openList[lowest_cost]);
+        //    openList[lowest_cost].setNeighbors(grid);
+        //}
+
         //Remove the evaluated node from the openList
-        remove_element(openList, openList[lowest_cost]);
+        remove_element(openList, evaluated_node);
+
+        //Add the evaluated node to the closedList
+        closedList.push(evaluated_node);
 
         //Obtain the neighbors of the node evaluated with the lowest cost
-        neighbors_list = openList[lowest_cost].neighbors;
+        var neighbors_list = evaluated_node.neighbors;
 
         for(var i = 0; i < neighbors_list.length; i++) {
-            if(!closedList.includes(neighbors_list[i])) {
-                tentative_g = openList[lowest_cost] + 1; //
+            neighbor = neighbors_list[i];
+            if(!closedList.includes(neighbor)) {
+                var tentative_g = evaluated_node.g + 1; //
                 //
-                if(openList.includes(neighbors_list[i])) {
+                if(openList.includes(neighbor)) {
                         //
-                        if(tentative_g < neighbors_list[i].g) {
-                            neighbors_list[i].g = tentative_g;
+                        if(tentative_g < neighbor.g) {
+                            neighbor.g = tentative_g;
                         }
                 } else {
                         //
-                        neighbors_list[i].g = tentative_g;
-                        openList.push(neighbors_list[i]);
+                        neighbor.g = tentative_g;
+                        openList.push(neighbor);
                 } // end if
+
+                //
+                neighbor.h = heuristic(neighbor, ending_node);
+                //
+                neighbor.f = neighbor.g + neighbor.h;
+                //
+                neighbor.p = evaluated_node;
             } // end if
         } // end for
-
-
     } else {
-        //Stop
+        // No Solution
     }
 
+    // Displays the background of the canvas grid as white
     for(var i = 0; i < rows; i++) {
         for(var j = 0; j < cols; j++) {
             // i and j represent the x and y coordinates of the nodes
@@ -137,18 +160,25 @@ function draw() {
         }
     }
 
-
+    // Display the nodes to be evaluated as green
     for(var i = 0; i < openList.length; i++) {
         openList[i].display(color(0,255,0));
     }
 
+    // Display the evaluated nodes as red
     for(var i = 0; i < closedList.length; i++) {
         closedList[i].display(color(255,0,0));
     }
+    path = [];
+    var temp  = evaluated_node;
+    path.push(temp);
+    while(temp.p) {
+        path.push(temp.p);
+        temp = temp.p;
+    }
 
-
-
-
-
-
-}
+    // Display the evaluated nodes as red
+    for(var i = 0; i < path.length; i++) {
+        path[i].display(color(0,0,255));
+    }
+} // end draw()
